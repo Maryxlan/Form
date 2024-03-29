24389ce8cd1fccab11d225a59ba859ecef25322c
Maria Laniewska <maria.laniewska@hotmail.com>
2024-03-29 22:44:16 +0100

    first commit

---
+++ main.php
@@ -0,0 +1,56 @@
+<?php
+
+require 'vendor/autoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   
+    $name = $_POST["name"];
+    $nachricht = $_POST["nachricht"];
+
+    // Empfänger-E-Mail-Adresse festlegen
+    $toEmail = "maria.lani96@gmail.com";
+
+    // Absender-E-Mail-Adresse festlegen
+    $fromEmail = $_POST["email"]; 
+
+    // Erstellen eines PHPMailer-Objekts
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'maria.lani96@gmail.com'; // Deine Hotmail-Adresse
+        $mail->Password   = '***'; // Dein Hotmail-Passwort
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+       // $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+
+   
+        $mail->setFrom($fromEmail, $name); // Absender aus dem Formular verwenden
+        $mail->addAddress($toEmail);
+     
+        $mail->isHTML(true);
+        $mail->Subject = 'Betreff';
+        $mail->Body    = $nachricht;
+
+        // E-Mail senden
+        if ($mail->send()) {
+            // Erfolg: Weiterleitung zur Erfolgsseite
+            header("Location: erfolg.html");
+            exit();
+        } else {
+            // Fehler beim Senden der E-Mail
+            echo "E-Mail konnte nicht gesendet werden: {$mail->ErrorInfo}";
+        }
+    } catch (Exception $e) {
+        echo "Ein Fehler ist aufgetreten: {$e->getMessage()}";
+    }
+} else {
+   
+    echo "Das Skript wurde nicht korrekt aufgerufen.";
+}
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'maria/form',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'maria/form' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'a0dcf5f7885a65221d34fcaddd96bb1a38a21635',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
